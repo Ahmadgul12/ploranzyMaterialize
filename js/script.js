7ae129c772c6799545a241391c75bc3d7f5cb423
ahmadgul <ag2732935@gmail.com>
2022-09-22 10:27:28 +0430

    modified[login_in_nav]

--- js/script.js
+++ js/script.js
@@ -19,8 +19,15 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
 
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
+
+$(document).ready(function () {
+  $('.modal').modal();
+});
